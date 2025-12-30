e0f96a3e1be180625790dadfedf931e0fc6d3ca7
Deepak Namdev <95995520+brightlight-sun@users.noreply.github.com>
2025-12-30 20:30:16 +0530

    login-register component added

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,51 +1,236 @@
-// name of a react comoponent always starts with capital letter
-import { useState } from "react"
-import { useNavigate } from "react-router"
-import { toast } from "react-toastify"
-import { registerUser } from "../services/users"
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import {toast} from "react-toastify"
+import { registerUser, findUserByEmail } from "../services/Users";
+import { Link } from "react-router";
+// import { registerUser } from "../services/users";
 
-function Register() {
+function Register()
+{
+
+    // firstName , lastName , email , password , phoneno , address
+    
+    // use formData object to maintain the state of the form fields
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-    })
-    const navigate = useNavigate()
-    const handleChange = (e) => setFormData({ ...formData, [e.target.id]: e.target.value })
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+    });
+    const [emailError, setEmailError] = useState("");
+    const [showPassword , setShowPassword] = useState(false);
+    const [passwordError, setPasswordError] = useState("");
+    const [passwordStrength, setPasswordStrength] = useState({
+        percent: 0,
+        color: "bg-danger"
+    });
+    
+ //   
+    const calculatePasswordStrength = (password) => {
+        let score = 0;
+    
+        if (password.length >= 8) score++;
+        if (/[A-Z]/.test(password)) score++;
+        if (/[a-z]/.test(password)) score++;
+        if (/\d/.test(password)) score++;
+        if (/[@$!%*?&]/.test(password)) score++;
+    
+        if (score <= 2) {
+            return { percent: 20, color: "bg-danger" };
+        } else if (score === 3 || score === 4) {
+            return { percent: 60, color: "bg-warning" };
+        } else {
+            return { percent: 100, color: "bg-success" };
+        }
+    };
+    
+//
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({ ...formData, [name]: value });
 
-    const handleOnClick = () => {
-        const userInfo = formData
-        // create function in js to handle submit
-        registerUser(userInfo)
-        toast.success("User Registered as"+ userInfo.name  )
-        navigate('/login')
-        
+        if(name === "email"){
+            const exixtingUser = findUserByEmail(value);
+            if(exixtingUser){
+                setEmailError("Email alreadty exists");
+            }else{
+                setEmailError("");
+            }
+        }
+        if(name === "password") {
+            setPasswordStrength(calculatePasswordStrength(value));
+            setPasswordError(validatePassword(value));
+        }
+    };
+//
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        if(passwordError) {
+            toast.error(passwordError);
+            return;
+        }
+        if (formData.password !== formData.confirmPassword) {
+            toast.error("Passwords do not match");
+            return;
+        }
+        try {
+            // register logic here
+            const user = await registerUser(formData)
+            toast.success(`User registered as : ${user.firstName}`)
+            navigate("/login");
+        }
+        catch(err) {
+            toast.error(err.message)
+        }
+    };
+    
+    // password validation 
+    const validatePassword = (password) => {
+        const passwordRegex = 
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if(!passsordRegex.test(password) ){
+            return "password must be atleast 8 char & include uppercase & special char"
+        }
+        return "";
     }
 
-    return <div className="container d-flex justify-content-center" style={{ width: "100%", height: "300px " }}>
+    //UI
+    return (
+        <div className="container" style={{marginTop:"10%"}}>
+        <div className="container d-flex  align-items-center border justify-content-center" style={{width:"50%", height:"500px", boxShadow:"1px 0px 5px"}}>
+            <center>
+            <center><h2>Register</h2></center>
+            <form onSubmit={handleRegister} style={{width: "110%"}} >
 
-        <div className="container d-flex flex-column justify-content-around align-items-center border border-1 shadow " style={{ width: "500px", height: "120%", marginTop: "90px" }}>
+                {/* first name */}
+                <div className="mb-3">
+                    <label htmlFor="firstName" className="form-label"></label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="firstName"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        placeholder="first name"
+                        required
+                    />
+                </div>
+            {/* last name */}
+                <div className="mb-3">
+                    {/* <label htmlFor="lastName" className="form-label">
+                        
+                    </label> */}
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="lastName"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        placeholder="last name"
+                        
+                    />
+                </div>
+                {/* email */}
+                <div className="mb-3">
+                    {/* <label htmlFor="email" className="form-label">
+                        
+                    </label> */}
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        placeholder="email"
+                        required
+                    />
+                    {/* live error message */}
+                    <small className="text-danger">{emailError}</small>
+                </div>
+                {/* password */}
+                <div className="mb-3 position-relative">
+                    {/* <label htmlFor="password" className="form-label"> </label> */}
 
-            <h3>Registration</h3>
-            <table className="form-group d-flex flex-column ">
-                <tbody>
-                    <tr className='form-group mb-3'>
-                        <td ><label htmlFor="name"></label></td>
-                        <td><input type="text" id="name" placeholder="enter your name" className="form-control" onChange={handleChange} /></td>
-                    </tr>
-                    <tr className='form-group mb-3'>
-                        <td><label htmlFor="email"></label></td>
-                        <td><input type="email" id="email" placeholder="enter your email" className="form-control" onChange={handleChange} /></td>
-                    </tr>
-                    <tr className='form-group mb-1'>
-                        <td><label htmlFor="password"></label></td>
-                        <td><input type="password" id="password" placeholder="enter your password" className="form-control" onChange={handleChange} /></td>
-                    </tr>
-                </tbody>
-            </table>
-            <button className="btn btn-success" onClick={handleOnClick}>Register</button>
+                    <input
+                        type={showPassword ? "text " : "password"}
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        placeholder="password"
+                    />
+                    <span
+                            onClick={() => setShowPassword(!showPassword)}
+                            style={{
+                                position: "absolute",
+                                right: "10px",
+                                top: "50%",
+                                transform: "translateY(-50%)",
+                                cursor: "pointer"
+                            }}
+                        >
+                            {showPassword ? "🙈" : "👁️"}
+                        </span>
+                        
+                        <div className="progress mt-1" style={{ height: "6px" }}>
+                        <div
+                                className={`progress-bar ${passwordStrength.color}`}
+                                style={{ width: `${passwordStrength.percent}%` }}
+                            ></div>
+                        </div>
+                    
+                </div>
+                {/* confirmPassword */}
+                <div className="mb-3 position-relative">
+                    {/* <label htmlFor="confirmPassword" className="form-label">
+                        
+                    </label> */}
+                    <input
+                        type={showPassword ? "text " : "password"}
+                        className="form-control"
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleChange}
+                        placeholder="confirm password"
+                    />
+                    <span
+                        onClick={() => setShowPassword(!showPassword)}
+                        style={{
+                            position: "absolute",
+                            right: "10px",
+                            top: "50%",
+                            transform: "translateY(-50%)",
+                            cursor: "pointer"
+                        }}>
+                        {showPassword ? "🙈" : "👁️"}
+                    </span>
+                </div>
+                
+                <div>
+                    <p>Already have account ?<Link to="/login" > login here</Link></p>
+                </div>
+
+                <button type="submit" 
+                        className="btn btn-primary"
+                        disabled={emailError !== ""}
+                        >
+                    Register
+                </button>
+            </form>
+            </center>
         </div>
-    </div>
-}
+        </div>
+    )
+}    
 
 export default Register
--- src/services/users.js
+++ src/services/users.js
@@ -1,5 +1,12 @@
 export function registerUser(newUser){
     const users =  JSON.parse(localStorage.getItem("users") || "[]")
+    //checks if email exists in local storage
+    const existingUser = users.find(
+        (u) => u.email.toLowerCase() === newUser.email.toLowerCase()
+    );
+    if(existingUser) {
+        throw new Error("Email already exists  - " + newUser.email)
+    }
 
     const lastId = users.at(-1)?.id || 0;
     newUser.id = lastId + 1;
@@ -13,6 +20,7 @@ export function findUserByEmail(email) {
 	users = users.filter((u) => u.email === email);
 	if (users.length > 0) return users[0];
 }
+
 
 export function findUserByCredentials({ email, password }) {
 	const user = findUserByEmail(email);
